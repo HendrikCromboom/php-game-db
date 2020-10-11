864e16f3f3b082ccb6c61f81f08deeadef8fda06
HENDRIK <hendrik@KALI.home>
2020-10-11 18:05:43 +0200

    added Achievement Class and basic properties

---
+++ ClassLibrary/Achievement.php
@@ -0,0 +1,8 @@
+<?php
+Class Achievement{
+    public string $gameParent;
+    public string $userParent;
+    public int $percentage;
+    public bool $platinum;
+}
+$thisAchievement = new Achievement();
