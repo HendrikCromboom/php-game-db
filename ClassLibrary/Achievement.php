1692212c6611f711a8c1575cbc45dddd13e0d3e4
HendrikCromboom <hcromboom@gmail.com>
2020-10-12 10:14:27 +0200

    before break

--- ClassLibrary/Achievement.php
+++ ClassLibrary/Achievement.php
@@ -4,5 +4,57 @@ Class Achievement{
     public string $userParent;
     public int $percentage;
     public bool $platinum;
+
+    public function __construct(string $gameParent, string $userParent, int $percentage, bool $platinum)
+    {
+        $this->gameParent = $gameParent;
+        $this->userParent = $userParent;
+        $this->percentage = $percentage;
+        $this->platinum = $platinum;
+    }
+
+    public function getGameParent()
+    {
+        return $this->gameParent;
+    }
+
+    public function setGameParent($gameParent)
+    {
+        $this->gameParent = $gameParent;
+    }
+
+    public function getUserParent()
+    {
+        return $this->userParent;
+    }
+
+    public function setUserParent($userParent)
+    {
+        $this->userParent = $userParent;
+    }
+
+    public function getPercentage()
+    {
+        return $this->percentage;
+    }
+
+    public function setPercentage($percentage)
+    {
+        $this->percentage = $percentage;
+    }
+
+    public function isPlatinum()
+    {
+        return $this->platinum;
+    }
+
+    public function setPlatinum($platinum)
+    {
+        $this->platinum = $platinum;
+    }
+
 }
-$thisAchievement = new Achievement();
+function createAchievement($gameParent, $userParent, $percentage, $platinum){
+    $thisAchievement = new Achievement($gameParent, $userParent, $percentage, $platinum );
+    array_push($achievementData, $thisAchievement);
+}
--- model.php
+++ model.php
@@ -3,6 +3,15 @@ include 'ClassLibrary/Achievement.php';
 include 'ClassLibrary/Game.php';
 include 'ClassLibrary/User.php';
 
-$achievementData = file_get_contents('achievementData.json');
-echo $achievementData;
+$achievementData = file_get_contents('Data/achievementData.json');
+$achievementData = json_decode($achievementData, true);
+$gameData = file_get_contents('Data/gameData.json');
+$gameData = json_decode($gameData, true);
+$userData = file_get_contents('Data/userData.json');
+$userData = json_decode($userData, true);
+
+
+
+
+
 
