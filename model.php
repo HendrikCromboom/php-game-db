7947ca06c31455c887ad45c962594d7e338b18d7
HENDRIK <hendrik@KALI.home>
2020-10-20 20:35:44 +0200

    added autoloader, removing requirements in model

--- model.php
+++ model.php
@@ -1,14 +1,6 @@
 <?php
-include 'ClassLibrary/Achievement.php';
-include 'ClassLibrary/Game.php';
-include 'ClassLibrary/User.php';
 
-$achievementData = file_get_contents('Data/achievementData.json');
-$achievementData = json_decode($achievementData, true);
-$gameData = file_get_contents('Data/gameData.json');
-$gameData = json_decode($gameData, true);
-$userData = file_get_contents('Data/userData.json');
-$userData = json_decode($userData, true);
+
 
 
 
